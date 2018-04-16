3044165d59db70e89ca9d0fe5382088d727033ea
Marty <mmammel12@gmail.com>
2018-04-15 22:17:20 -0400

    validate addition limits added

--- marty.js
+++ marty.js
@@ -4,9 +4,43 @@ Marty Mammel
 */
 "use strict";
 
-// TODO validate username
-function validateUsername(usernameInput) {
+/*
+##     ##  ######  ######## ########  ##    ##    ###    ##     ## ########
+##     ## ##    ## ##       ##     ## ###   ##   ## ##   ###   ### ##
+##     ## ##       ##       ##     ## ####  ##  ##   ##  #### #### ##
+##     ##  ######  ######   ########  ## ## ## ##     ## ## ### ## ######
+##     ##       ## ##       ##   ##   ##  #### ######### ##     ## ##
+##     ## ##    ## ##       ##    ##  ##   ### ##     ## ##     ## ##
+ #######   ######  ######## ##     ## ##    ## ##     ## ##     ## ########
+*/
+
+var User = { name: "" };
+
+function createUser() {
   var usernameInput = document.getElementById("usernameInput").value;
+  if (validateUsername(usernameInput)) {
+    // Create user object
+    User.createdDate = new Date();
+    User.name = usernameInput;
+    User.addCorrect = 0;
+    User.addIncorrect = 0;
+    User.subCorrect = 0;
+    User.subIncorrect = 0;
+    User.multCorrect = 0;
+    User.multIncorrect = 0;
+    User.divCorrect = 0;
+    User.divIncorrect = 0;
+
+    document.getElementById("username").innerHTML = User.name;
+
+    // Hide input, show username
+    document.getElementById("inputData").style.display = "none";
+    document.getElementById("displayData").style.display = "block";
+    document.getElementById("username").style.display = "block";
+  }
+}
+
+function validateUsername(usernameInput) {
   var validInputs = /^[0-9a-zA-Z_\s]+$/;
   var errorDiv = document.querySelector("#inputData .errorMessage");
   try {
@@ -36,52 +70,127 @@ function clearUsername() {
   errorDiv.style.display = "none";
 }
 
+/*
+   ###    ########  ########  #### ######## ####  #######  ##    ##
+  ## ##   ##     ## ##     ##  ##     ##     ##  ##     ## ###   ##
+ ##   ##  ##     ## ##     ##  ##     ##     ##  ##     ## ####  ##
+##     ## ##     ## ##     ##  ##     ##     ##  ##     ## ## ## ##
+######### ##     ## ##     ##  ##     ##     ##  ##     ## ##  ####
+##     ## ##     ## ##     ##  ##     ##     ##  ##     ## ##   ###
+##     ## ########  ########  ####    ##    ####  #######  ##    ##
+*/
+
+function addValidateLimits() {
+  var addLowerLimit = document.getElementById("addLowerLimit").value;
+  var addUpperLimit = document.getElementById("addUpperLimit").value;
+  var leftNumber = document.getElementById("addProblemLeft");
+  var rightNumber = document.getElementById("addProblemRight");document.getElementById("addProblemLeft")
+  var errorDiv = document.querySelector("#addition .errorMessage");
+  errorDiv.style.display = "none";
+
+  try {
+    if (addLowerLimit != "" && addUpperLimit != "") {
+      if (!addLowerLimit.match(/^[0-9]+$/)) {
+        throw "Lower Limit must be an integer";
+      } else if (!addUpperLimit.match(/^[0-9]+$/)) {
+        throw "Upper Limit must be an integer";
+      } else { // numbers in both text boxes, convert to int
+        var lowerLimit = parseInt(addLowerLimit);
+        var upperLimit = parseInt(addUpperLimit);
+
+        if (lowerLimit < upperLimit) {
+          leftNumber.innerHTML = "";
+          leftNumber.innerHTML = Math.floor(Math.random() * (upperLimit - lowerLimit)) + lowerLimit;
+          rightNumber.innerHTML = "";
+          rightNumber.innerHTML = Math.floor(Math.random() * (upperLimit - lowerLimit)) + lowerLimit;
+        } else if (lowerLimit >= upperLimit) {
+          throw "Lower Limit must be less than Upper Limit"
+        }
+      }
+    }
+  }
+  catch (msg) {
+    errorDiv.innerHTML = msg;
+    errorDiv.style.display = "block";
+  }
+}
+
+function checkAddAnswer() {
+  //TODO check addition answer
+}
+
+/*
+######## ##     ## ######## ##    ## ######## ##       ####  ######  ######## ######## ##    ## ######## ########   ######
+##       ##     ## ##       ###   ##    ##    ##        ##  ##    ##    ##    ##       ###   ## ##       ##     ## ##    ##
+##       ##     ## ##       ####  ##    ##    ##        ##  ##          ##    ##       ####  ## ##       ##     ## ##
+######   ##     ## ######   ## ## ##    ##    ##        ##   ######     ##    ######   ## ## ## ######   ########   ######
+##        ##   ##  ##       ##  ####    ##    ##        ##        ##    ##    ##       ##  #### ##       ##   ##         ##
+##         ## ##   ##       ##   ###    ##    ##        ##  ##    ##    ##    ##       ##   ### ##       ##    ##  ##    ##
+########    ###    ######## ##    ##    ##    ######## ####  ######     ##    ######## ##    ## ######## ##     ##  ######
+*/
+
 function createEventListeners() {
   /// variables to store buttons
   // username buttons
   var unAccept = document.getElementById("usernameAcceptBtn");
   var unClear = document.getElementById("usernameClearBtn");
   // addition buttons
-  var addCheck = document.getElementById("addCheckBtn");
-  var addAnswer = document.getElementById("addAnswerBtn");
-  var addNext = document.getElementById("addNextBtn");
+  var addCheck = document.getElementById("addCheckBtn"); //TODO
+  var addAnswer = document.getElementById("addAnswerBtn"); //TODO
+  var addNext = document.getElementById("addNextBtn"); //TODO
   // subtraction buttons
-  var subCheck = document.getElementById("subCheckBtn");
-  var subAnswer = document.getElementById("subAnswerBtn");
-  var subNext = document.getElementById("subNextBtn");
+  var subCheck = document.getElementById("subCheckBtn"); //TODO
+  var subAnswer = document.getElementById("subAnswerBtn"); //TODO
+  var subNext = document.getElementById("subNextBtn"); //TODO
   // multiplication buttons
-  var multCheck = document.getElementById("multCheckBtn");
-  var multAnswer = document.getElementById("multAnswerBtn");
-  var multNext = document.getElementById("multNextBtn");
+  var multCheck = document.getElementById("multCheckBtn"); //TODO
+  var multAnswer = document.getElementById("multAnswerBtn"); //TODO
+  var multNext = document.getElementById("multNextBtn"); //TODO
   // division buttons
-  var divCheck = document.getElementById("divCheckBtn");
-  var divAnswer = document.getElementById("divAnswerBtn");
-  var divNext = document.getElementById("divNextBtn");
+  var divCheck = document.getElementById("divCheckBtn"); //TODO
+  var divAnswer = document.getElementById("divAnswerBtn"); //TODO
+  var divNext = document.getElementById("divNextBtn"); //TODO
   // collatz button
-  var collatzCheck = document.getElementById("collatzCheck");
+  var collatzCheck = document.getElementById("collatzCheck"); //TODO
+
   /// variables to store text boxes
   // addition text boxes
   var addLowerLimit = document.getElementById("addLowerLimit");
   var addUpperLimit = document.getElementById("addUpperLimit");
   // subtraction text boxes
-  var subLowerLimit = document.getElementById("subLowerLimit");
-  var subUpperLimit = document.getElementById("subUpperLimit");
+  var subLowerLimit = document.getElementById("subLowerLimit"); //TODO
+  var subUpperLimit = document.getElementById("subUpperLimit"); //TODO
   // multiplication text boxes
-  var multLowerLimit = document.getElementById("multLowerLimit");
-  var multUpperLimit = document.getElementById("multUpperLimit");
+  var multLowerLimit = document.getElementById("multLowerLimit"); //TODO
+  var multUpperLimit = document.getElementById("multUpperLimit"); //TODO
   // division text boxes
-  var divLowerLimit = document.getElementById("divLowerLimit");
-  var divUpperLimit = document.getElementById("divUpperLimit");
+  var divLowerLimit = document.getElementById("divLowerLimit"); //TODO
+  var divUpperLimit = document.getElementById("divUpperLimit"); //TODO
 
   if (unAccept.addEventListener) {
-    // TODO change validateUsername call to something else
-    // validateUsername should be called from something that
-    // wants boolean return
-    unAccept.addEventListener("click", validateUsername, false);
+    /// buttons
+    // username buttons
+    unAccept.addEventListener("click", createUser, false);
     unClear.addEventListener("click", clearUsername, false);
+    // addition buttons
+    addCheck.addEventListener("click", checkAddAnswer, false);
+
+    /// text boxes
+    // addition text boxes
+    addLowerLimit.addEventListener("change", addValidateLimits, false);
+    addUpperLimit.addEventListener("change", addValidateLimits, false);
   } else if (unAccept.attachEvent) {
-    unAccept.attachEvent("onclick", validateUsername);
+    /// buttons
+    // username buttons
+    unAccept.attachEvent("onclick", createUser);
     unClear.attachEvent("onclick", clearUsername);
+    // addition buttons
+    addCheck.attachEvent("onclick", checkAddAnswer);
+
+    /// text boxes
+    // addition text boxes
+    addLowerLimit.attachEvent("onchange", addValidateLimits, false);
+    addUpperLimit.attachEvent("onchange", addValidateLimits, false);
   }
 }
 
